51eb76830d24d2544d5510ae9e370a5a3a334f25
briankeefe <briankeefe@umass.edu>
2019-02-25 16:25:21 -0500

    Added textbox

---
+++ todo.js
@@ -0,0 +1,10 @@
+document.getElementById("add").onclick = function () {
+    //First things first, we need our text:
+    var text = document.getElementById("idea").value; //.value gets input values
+
+    //Now construct a quick list element
+    var li = "<li>" + text + "</li>";
+
+    //Now use appendChild and add it to the list!
+    document.getElementById("list").appendChild(li);
+}
