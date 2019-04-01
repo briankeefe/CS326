d1c712f8cea2b2eca19d05a16bc84f629ef335c1
Kaputnick <chuckyfreddyjason@gmail.com>
2019-04-01 16:44:13 -0400

    server.js

--- server.js
+++ server.js
@@ -7,7 +7,42 @@ app.use(express.static('static'));
 app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
+// LOOK OVER THIS CODE
+app.get('/api/issues', (req, res) => {
+    db.collection('issues').find().toArray().then(issues => {
+      const metadata = { total_count: issues.length };
+      res.json({ _metadata: metadata, records: issues })
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({ message: `Internal Server Error: ${error}` });
+    });
+  });
+  
+  app.post('/api/issues', (req, res) => {
+    const newIssue = req.body;
+    newIssue.created = new Date();
+    if (!newIssue.status)
+      newIssue.status = 'New';
+  
+    const err = validateIssue(newIssue);
+    if (err) {
+      res.status(422).json({ message: `Invalid request: ${err}` });
+      return;
+    }
+  
+    db.collection('issues').insertOne(newIssue).then(result =>
+      db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
+    ).then(newIssue => {
+      res.json(newIssue);
+    }).catch(error => {
+      console.log(error);
+      res.status(500).json({ message: `Internal Server Error: ${error}` });
+    });
+  });
+  
+  
 
+//
 //old code
 //app.listen(3000, function () {
 //    console.log('App started on port 3000');
