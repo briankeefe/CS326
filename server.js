5edd41cf21b8a74fc09e6e9f5c8c12f92249b44f
Kaputnick <chuckyfreddyjason@gmail.com>
2019-04-04 19:22:18 -0400

    edited post and get, they may work but am unsure how to test

--- server.js
+++ server.js
@@ -2,19 +2,20 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use(express.static('static'));
 app.use(bodyParser.json());
 
 const MongoClient = require('mongodb').MongoClient;
+
+
 // LOOK OVER THIS CODE
+//I changed records to assets, but im unsure how to test to see if its working. 
 app.get('/api/issues', (req, res) => {
     db.collection('issues').find().toArray().then(issues => {
       const metadata = { total_count: issues.length };
-      res.json({ _metadata: metadata, records: issues })
+      res.json({ _metadata: metadata, assets: issues })
     }).catch(error => {
       console.log(error);
       res.status(500).json({ message: `Internal Server Error: ${error}` });
@@ -25,12 +26,12 @@ app.post('/api/issues', (req, res) => {
     newIssue.created = new Date();
     if (!newIssue.status)
         newIssue.status = 'New';
-    
-        const err = validateIssue(newIssue);
-        if (err) {
-            res.status(422).json({ message: `Invalid request: ${err}` });
-            return;
-        }
+    // we do not need this as of now, posibly implement a valid entry later
+       // const err = validateIssue(newIssue);
+        //if (err) {
+         //   res.status(422).json({ message: `Invalid request: ${err}` });
+          //  return;
+       // }
     
         db.collection('issues').insertOne(newIssue).then(result =>
         db.collection('issues').find({ _id: result.insertedId }).limit(1).next()
@@ -49,9 +50,11 @@ app.post('/api/issues', (req, res) => {
 //app.listen(3000, function () {
 //    console.log('App started on port 3000');
 //});
+
+//I changed the db to SaveMe
 let db;
 MongoClient.connect('mongodb://localhost', { useNewUrlParser: true }).then(connection => {
-  db = connection.db('issuetracker');
+  db = connection.db('SaveMe');
   app.listen(3000, () => {
     console.log('App started on port 3000');
   });
